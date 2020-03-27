0474038a95130cc7b981aacb091653bd4c2e6504
Madhur817 <Mkaliafbd@gmail.com>
2020-03-27 21:25:10 +0530

    Add files via upload

---
+++ build/precache-manifest.940a3ab0c88d995db14e1827571772c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84dcd90d6a46b6a534b9a799a01d0a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "969fd13f25907a1e7a51",
+    "url": "/static/css/main.aa45bae2.chunk.css"
+  },
+  {
+    "revision": "b595e528c191c457dc55",
+    "url": "/static/js/2.c9c5730c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9c5730c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969fd13f25907a1e7a51",
+    "url": "/static/js/main.e2c92e65.chunk.js"
+  },
+  {
+    "revision": "9e8f2ab24fbeacb4e357",
+    "url": "/static/js/runtime-main.abe37d6a.js"
+  },
+  {
+    "revision": "dae6d02fe96a9e5a09a8ac33f85ffd11",
+    "url": "/static/media/final.dae6d02f.svg"
+  },
+  {
+    "revision": "49fad9e6bfd5a3db8e62ba07fe0496d9",
+    "url": "/static/media/start.49fad9e6.svg"
+  }
+]);
